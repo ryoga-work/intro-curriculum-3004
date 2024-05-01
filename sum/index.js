648cab8e0fc926618a444d86a127527f66bd6832
ryoga-work <kubota.ryoga.work@gmail.com>
2024-05-01 13:58:02 +0900

    マルチの追加

--- sum/index.js
+++ sum/index.js
@@ -10,5 +10,12 @@ function add(numbers) {
   }
   return result;
 }
+function multi(numbers) {
+  let result = 1;
+  for (const num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
 
-module.exports = { add };
+module.exports = { add,multi };
